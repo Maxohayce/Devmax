f430485099fb189e5f3628b177daabb9d11dedf1
Maxohayce <devmax.corl@gmail.com>
2023-09-26 17:55:05 +0100

    new commit

--- src/components/PlaySound/PlaySound.jsx
+++ src/components/PlaySound/PlaySound.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import Sound from "react-sound";
-// import SpotifyPlayer from 'react-spotify-player';
+import SpotifyPlayer from 'react-spotify-player';
 import "./playSound.css";
 
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -26,17 +25,21 @@ const PlaySound = () => {
         handler();
     }, [])
 
-    const style = {
-        borderRadius: 12,
-        width: 300,
-        height: 300,
-        position: "absolute",
-    }
-
     return (
         <div className="musicPlayer">
-            <iframe title="playlist" style={style} src="https://open.spotify.com/embed/playlist/1FM8v5HPJ7GoyMoo5SjOJj?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            {/* <iframe title="playlist" style={style} src="https://open.spotify.com/embed/playlist/1FM8v5HPJ7GoyMoo5SjOJj?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> */}
 
+            <SpotifyPlayer
+                uri={spotifyURI}
+                size={{
+                    width: 300,
+                    height: 300,
+                }}
+                view={"list"}
+                theme={"white"}
+                allow={"autoplay"}
+
+            />
         </div>
     );
 }
--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { PlaySound } from "../../components";
+import { Link } from "react-router-dom";
 import "./header.css";
 
 const Header = () => {
-    
+
     return (
         <div className="intro">
             <div className="intro-text">
-                <h1>Hi <br/> I'm <b>Max</b>well, <br/> I build websites</h1>
+                <h1>Hi <br /> I'm <b>Max</b>well, <br /> I build websites</h1>
                 <i>FullStack developer</i>
-                <button>Contact me!</button>
+                <Link to={"/contact"}>
+                    <button>Contact me!</button>
+                </Link>
             </div>
             <PlaySound />
         </div>
